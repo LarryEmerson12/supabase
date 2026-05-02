e4f5cec8f5b2d162a926c2da877df13772208fa2
Larry Emerson <larry.emerson14@gmail.com>
2026-05-02 16:43:00 +0800

    REdeco

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,54 @@
-import './index.css'
-import { useState, useEffect } from 'react'
-import { supabase } from './utils/supabase'
+import "./index.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./utils/supabase";
 
 export default function App() {
-  const [todos, setTodos] = useState<any[]>([])
+  const [todos, setTodos] = useState<any[]>([]);
 
   useEffect(() => {
     async function getTodos() {
-      const { data: todos } = await supabase.from('todos').select()
+      const { data: todos } = await supabase.from("todos").select();
 
       if (todos) {
-        setTodos(todos)
+        setTodos(todos);
       }
     }
 
-    getTodos()
-  }, [])
+    getTodos();
+  }, []);
 
   return (
     <main>
       <h1>Supabase + Vite + React 🌱</h1>
-      <h2>Tasks for today:</h2>
-      {todos.map((todo) => (
-        <ul key={todo.id}>{todo.name}{todo.emoji}</ul>
-      ))}
+      <section>
+        <h2>Tasks for today:</h2>
+        {todos.map((todo) => (
+          // old approach
+          // <ul key={todo.id}>
+          //   {todo.name}
+          //   {todo.emoji}
+          // </ul>
+
+          <ul key={todo.id}>
+            <input type="checkbox" />
+            <label htmlFor="checkbox">
+              {todo.name}
+              {todo.emoji}
+            </label>
+          </ul>
+        ))}
+      </section>
+      <section>
+        <h2>How does this work?</h2>
+        <p>
+          This file loads data from a Supabase database called{" "}
+          <code>todos</code>. Then, it displays the name and emoji for each task
+          in a list.
+        </p>
+      </section>
+      <footer>
+        <h4>Made with ❤️ by Larry</h4>
+      </footer>
     </main>
-  )
+  );
 }
